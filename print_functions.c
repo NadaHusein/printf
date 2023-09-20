28e3d56c4302721ced98fbb73a9f6ff866939c1b
sweilam <boodyhussein12@gmail.com>
2023-09-19 18:28:24 -0700

    update

--- print_functions.c
+++ print_functions.c
@@ -7,15 +7,15 @@
  */
 int print_char(va_list ap, params_t *params)
 {
-  char pad_char = ' ';
-  unsigned int pad = 1, sum = 0, ch=va_arg(ap, int);
-  if (params->minus_flag)
-    sum += _putchar(ch);
-  while (pad++ < params->width)
-    sum += _putchar(pad_char);
-  if (!params->minus_flag)
-    sum += _putchar(ch);
-  return (sum);
+char pad_char = ' ';
+unsigned int pad = 1, sum = 0, ch=va_arg(ap, int);
+if (params->minus_flag)
+sum += _putchar(ch);
+while (pad++ < params->width)
+sum += _putchar(pad_char);
+if (!params->minus_flag)
+sum += _putchar(ch);
+return (sum);
 }
 /**
  * print_int prints integer
